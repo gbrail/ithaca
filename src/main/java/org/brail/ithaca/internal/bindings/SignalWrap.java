05b38274ba5a27f8a512eeef6c6605144afcc79f
Greg Brail <gbrail@users.noreply.github.com>
2026-07-23 22:32:57 -0400

    Signal handling implemented but not working

--- src/main/java/org/brail/ithaca/internal/bindings/SignalWrap.java
+++ src/main/java/org/brail/ithaca/internal/bindings/SignalWrap.java
@@ -2,10 +2,7 @@ package org.brail.ithaca.internal.bindings;
 
 import org.brail.ithaca.internal.Environment;
 import org.brail.ithaca.internal.handles.SignalHandle;
-import org.mozilla.javascript.Context;
-import org.mozilla.javascript.LambdaConstructor;
-import org.mozilla.javascript.Scriptable;
-import org.mozilla.javascript.VarScope;
+import org.mozilla.javascript.*;
 
 public class SignalWrap {
   public static Scriptable init(Environment e, Context cx, VarScope s) {
@@ -13,6 +10,13 @@ public class SignalWrap {
     HandleWrap.initializeConstructor(signal, s);
     signal.definePrototypeMethod(s, "start", 0, SignalHandle::js_start);
     signal.definePrototypeMethod(s, "stop", 0, SignalHandle::js_stop);
+    signal.definePrototypeProperty(
+        cx,
+        "onsignal",
+        SignalHandle::get_onSignal,
+        SignalHandle::set_onSignal,
+        ScriptableObject.DONTENUM);
+
     var o = cx.newObject(s);
     o.put("Signal", o, signal);
     return o;
--- src/main/java/org/brail/ithaca/internal/handles/SignalHandle.java
+++ src/main/java/org/brail/ithaca/internal/handles/SignalHandle.java
@@ -1,15 +1,17 @@
 package org.brail.ithaca.internal.handles;
 
 import org.brail.ithaca.internal.Environment;
-import org.mozilla.javascript.Context;
-import org.mozilla.javascript.Scriptable;
-import org.mozilla.javascript.Undefined;
-import org.mozilla.javascript.VarScope;
+import org.brail.ithaca.internal.bindings.NodeConstants;
+import org.brail.ithaca.internal.common.ArgUtils;
+import org.mozilla.javascript.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class SignalHandle extends Handle {
   private static final Logger log = LoggerFactory.getLogger(SignalHandle.class);
+
+  private Thread hook;
+  private Callable onSignal;
 
   public SignalHandle(Environment env) {
     super(env);
@@ -18,6 +20,10 @@ public class SignalHandle extends Handle {
   @Override
   public String getClassName() {
     return "Signal";
+  }
+
+  private static SignalHandle realThis(Object to) {
+    return LambdaConstructor.convertThisObject(to, SignalHandle.class);
   }
 
   @Override
@@ -31,12 +37,46 @@ public class SignalHandle extends Handle {
   }
 
   public static Object js_start(Context cx, VarScope s, Object to, Object[] args) {
-    log.debug("start");
-    return Undefined.instance;
+    ArgUtils.checkArgs(1, args);
+    var self = realThis(to);
+    int sig = ScriptRuntime.toInt32(args[0]);
+    if (sig != NodeConstants.Signals.SIGINT && sig != NodeConstants.Signals.SIGTERM) {
+      log.warn("Ignoring attempt to listen for signal {}", sig);
+      return 0;
+    }
+    log.debug("Installing shutdown hook for signal {}", sig);
+    self.hook =
+        new Thread(
+            () -> {
+              self.environment.deliverCallback(
+                  (lcx) -> {
+                    log.debug("Delivering shutdown hook for {}", sig);
+                    self.onSignal.call(lcx, s, null, ScriptRuntime.emptyArgs);
+                  });
+            },
+            "Ithaca Shutdown Hook");
+    Runtime.getRuntime().addShutdownHook(self.hook);
+    return 0;
   }
 
   public static Object js_stop(Context cx, VarScope s, Object to, Object[] args) {
-    log.debug("stop");
+    var self = realThis(to);
+    if (self.hook != null) {
+      Runtime.getRuntime().removeShutdownHook(self.hook);
+      self.hook = null;
+    }
+    return 0;
+  }
+
+  public static Object get_onSignal(Scriptable to) {
+    var self = realThis(to);
+    if (self.onSignal != null) {
+      return self.onSignal;
+    }
     return Undefined.instance;
+  }
+
+  public static void set_onSignal(Scriptable to, Object o) {
+    realThis(to).onSignal = (Callable) o;
   }
 }
